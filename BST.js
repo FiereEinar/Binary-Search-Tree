1b30ec36d500091deee7d875d130f0806e230524
FiereEinar <nickxylanmelloria@gmail.com>
2024-01-20 08:32:36 +0800

    initial commit

---
+++ BST.js
@@ -0,0 +1,54 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree {
+    constructor(array) {
+        this.array = array;
+        this.root = this.buildTree(this.array, 0, this.array.length - 1);
+    }
+
+    buildTree(array, start, end) {
+        if (start > end) return null;
+
+        const mid = parseInt((start + end) / 2);
+        const node = new Node(array[mid]);
+
+
+        node.left = this.buildTree(array, start, mid - 1);
+        node.right = this.buildTree(array, mid + 1, end);
+
+        return node;
+    }
+
+    insert() {
+
+    }
+
+    delete() {
+
+    }
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+        return;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
+const myArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const myTree = new Tree(myArray);
+
+prettyPrint(myTree.root);
